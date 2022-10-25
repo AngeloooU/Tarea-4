0d3b25c948124b4cb3e1b4b8a60b52ab9159c397
AngeloooU <80928379+AngeloooU@users.noreply.github.com>
2022-10-24 23:41:19 -0300

    first commit

---
+++ promedio.js
@@ -0,0 +1,19 @@
+// Ciclo para sumar notas y promediarlas//
+
+var notas;
+var promedio;
+var suma=0;
+
+
+var nnotas= parseInt(prompt("Ingresa la cantidad de notas a promediar"))
+
+    for(i=0; i < nnotas; i++){
+        var notas= parseInt(prompt("Ingresa tu nota " + i));
+        suma = suma + notas;
+
+}
+
+promedio = suma/ nnotas;
+
+alert("La suma de tus notas es " + suma);
+alert("El promedio de tus notas es " + promedio);
