a322837a8cffa704ade1aa9d0c249956188992a1
AngeloooU <80928379+AngeloooU@users.noreply.github.com>
2022-10-24 23:55:00 -0300

    update

--- promedio.js
+++ promedio.js
@@ -5,15 +5,15 @@ var promedio;
 var suma=0;
 
 
-var nnotas= parseInt(prompt("Ingresa la cantidad de notas a promediar"))
+var notas= parseInt(prompt("Ingresa la cantidad de notas a promediar"))
 
-    for(i=0; i < nnotas; i++){
+    for(i=0; i < notas; i++){
         var notas= parseInt(prompt("Ingresa tu nota " + i));
         suma = suma + notas;
 
 }
 
-promedio = suma/ nnotas;
+promedio = suma/ notas;
 
 alert("La suma de tus notas es " + suma);
 alert("El promedio de tus notas es " + promedio);
